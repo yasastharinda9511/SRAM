f140891eb29f4c015966f9ca9f9e35b7ded616cd
Yasas Tharinda <37435024+yasastharinda9511@users.noreply.github.com>
2019-04-12 13:53:36 +0530

    Update ram_test.v

--- ram_test.v
+++ ram_test.v
@@ -1,19 +1,24 @@
-module ram_test(clk,write_en,bus,data_in,data_out,output_enable,data_enable,chip_en,UB,LB);
-	input clk;
+module ram_test(write_en,bus,data_in,data_out,output_enable,data_enable,chip_enable,UB,LB);
 	input [1:0] write_en;
-	output reg output_enable;
+	/*output reg output_enable;
 	output reg data_enable;
 	output reg chip_en;
 	output reg UB;
-	output reg LB;
+	output reg LB;*/
 	inout [15:0] bus;
 	//inout [15:0] bus;
 	input [15:0] data_in;
 	output [15:0]data_out;
 	
+	
+	output output_enable;
+	output data_enable;
+	output chip_enable;
+	output UB;
+	output LB;
 	//output [15:0]data_out;
 	
-	always@(posedge clk)
+	/*always@(posedge clk)
 		begin
 			if(write_en==2'b11)
 				begin
@@ -32,7 +37,7 @@ module ram_test(clk,write_en,bus,data_in,data_out,output_enable,data_enable,chip
 					LB= 1'b0;
 					UB= 1'b0;
 				end
-			/*else 
+			else 
 				begin
 					data_enable=1'b1;
 					chip_en=1'b1;
@@ -40,8 +45,13 @@ module ram_test(clk,write_en,bus,data_in,data_out,output_enable,data_enable,chip
 					LB= 1'bz;
 					UB= 1'bz;
 					//bus=16'bzzzzzzzzzzzzzzzz;
-				end*/
-		end
+				end
+		end*/
+		assign data_enable=(write_en==2'b11) ? 1'b0 :1'b1 ;
+		assign chip_enable=(write_en==2'b11 || write_en==2'b00) ? 1'b0 :1'b1 ;
+		assign output_enable=(write_en==2'b00) ? 1'b0 :1'b1 ;
+		assign LB= (write_en==2'b11 || write_en==2'b00) ? 1'b0 :1'b1 ;
+		assign UB= (write_en==2'b11 || write_en==2'b00) ? 1'b0 :1'b1 ;
 	
 		assign data_out=(write_en ==2'b00) ? bus : 16'dz;
 		assign bus= (write_en ==2'b11) ? data_in: 16'dz;
